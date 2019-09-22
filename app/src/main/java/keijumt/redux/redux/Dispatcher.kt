6b0369716d6931e1d68be5c9037c27cac908bf4e
keijumt <keijumt@gmail.com>
2019-09-22 18:34:57 +0900

    Reformat code

--- app/src/main/java/keijumt/redux/redux/Dispatcher.kt
+++ app/src/main/java/keijumt/redux/redux/Dispatcher.kt
@@ -1,7 +1,8 @@
 package keijumt.redux.redux
 
-class Dispatcher
-    (private val store: Store) {
+class Dispatcher(
+    private val store: Store
+) {
     fun dispatch(action: Action) {
         store.dispatch(action)
     }
